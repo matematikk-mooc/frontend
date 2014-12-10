0657c8db8cd6affbdf5d9a2d405676bbd2071241
Kjell-Magne Øierud <kjellm@oierud.net>
2014-12-10 13:34:04 +0100

    Refactor API code

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -3,36 +3,46 @@ this.mmooc=this.mmooc||{};
 
 this.mmooc.api = function() {
     return {
-        uri_prefix: "/api/v1",
+        _uriPrefix: "/api/v1",
 
-        default_error: function (event, jqxhr, settings, thrownError) {
+        _defaultError: function (event, jqxhr, settings, thrownError) {
             console.log(event, jqxhr, settings, thrownError);
         },
 
-        getEnrolledCourses: function(callback, error) {
-            error = error || this.default_error;
-            var uri = this.uri_prefix + "/courses";
-            var params = { "include": ["syllabus_body" , "course_progress"] };
+        _get: function(options) {
+            var error    = options.error || this.defaultError;
+            var uri      = this.uriPrefix + options.uri;
+            var params   = options.params || {};
+            var callback = options.callback;
             $.get(uri, params, callback).fail(error);
         },
 
+        getEnrolledCourses: function(callback, error) {
+            this._get({
+                "callback": callback,
+                "error":    error,
+                "uri":      "/courses",
+                "params":   { "include": ["syllabus_body" , "course_progress"] }
+            });
+        },
+
+        /* FIXME Regarding include items: This parameter suggests that
+         * Canvas return module items directly in the Module object
+         * JSON, to avoid having to make separate API requests for
+         * each module when enumerating modules and items. Canvas is
+         * free to omit 'items' for any particular module if it deems
+         * them too numerous to return inline. Callers must be
+         * prepared to use the List Module Items API if items are not
+         * returned.
+         */
         getModulesForCurrentCourse: function(callback, error) {
-            error = error || this.default_error;
             var courseId = ENV.COURSE_ID;
-            var uri = this.uri_prefix + "/courses/" + courseId + "/modules";
-            var params = { "include": ["items"] };
-            /* FIXME
-             * Regarding include items:
-             * This parameter suggests that Canvas return module items
-             * directly in the Module object JSON, to avoid having to
-             * make separate API requests for each module when
-             * enumerating modules and items. Canvas is free to omit
-             * 'items' for any particular module if it deems them too
-             * numerous to return inline. Callers must be prepared to
-             * use the List Module Items API if items are not
-             * returned.
-             */
-            $.get(uri, params, callback).fail(error);
+            this._get({
+                "callback": callback,
+                "error":    error,
+                "uri":      "/courses/" + courseId + "/modules",
+                "params":   { "include": ["items"] }
+            });
         }
     };
 }();
