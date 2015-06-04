182141260def54969c3a616e6b5b297bd503efc3
Claudio Bartoli <claudio.bartoli@kantega.no>
2015-06-04 13:56:27 +0200

    MAT-190: remove unused code

--- src/js/modules/badges.js
+++ src/js/modules/badges.js
@@ -2,20 +2,6 @@ this.mmooc=this.mmooc||{};
 
 
 this.mmooc.badges = function() {
-
-    var _frame_document = document.getElementById('tool_content').contentWindow.document;
-    var $badges = $(_frame_document).find('#badge-wrap .row-fluid .span4');
-    $badges.each(function (){
-        $(this).hide();
-        transformAndShow(this);
-    });
-
-
-    var transformAndShow = function (badgeView) {
-
-    };
-
-
     return {
         initPage: function() {
             mmooc.util.adaptHeghtToIframeContentForId('tool_content');
