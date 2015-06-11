c03bcc028960fe04e2d1ff3e29e99968180920eb
Vegar Norman <vegar.norman@kantega.no>
2015-06-11 15:48:42 +0200

    Added tests to spec

--- spec/groups_spec.js
+++ spec/groups_spec.js
@@ -7,11 +7,15 @@ describe("groups", function() {
   describe("checkGroupPageLink", function() {
     it("Group page links should only be corrected when on the groups page itself", function() {
       expect(groups.changeGroupListURLs(baseURL + "/groups")).toBe(true);
+      expect(groups.changeGroupListURLs(baseURL + "/groups/")).toBe(true);
       expect(groups.changeGroupListURLs(baseURL + "/groups/1")).toBe(false);
+      expect(groups.changeGroupListURLs(baseURL + "/groups/1/")).toBe(false);
       expect(groups.changeGroupListURLs(baseURL + "/groups?foo=bar")).toBe(true);
       expect(groups.changeGroupListURLs(baseURL + "/groups/1?foo=bar")).toBe(false);
       expect(groups.changeGroupListURLs(baseURL + "/groups?foo=bar&bar=baz")).toBe(true);
       expect(groups.changeGroupListURLs(baseURL + "/groups/1?foo=bar&bar=baz")).toBe(false);
+      expect(groups.changeGroupListURLs(baseURL + "/groups/1/foo/bar/")).toBe(false);
+      expect(groups.changeGroupListURLs(baseURL + "/groups/1/foo/bar")).toBe(false);
     });
   });
 });
