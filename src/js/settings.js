07f7eb10acb87a50ad3bec91a9200d28883b2201
Stian Hågensen <stian.hagensen@bouvet.no>
2016-12-13 13:39:27 +0100

    Comma fix

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,7 +4,7 @@ this.mmooc=this.mmooc||{};
 this.mmooc.settings = {
     'CanvaBadgeProtocolAndHost' : 'https://canvabadges-beta-iktsenteret.bibsys.no',
     'useCanvaBadge' : true,
-    'defaultNumberOfReviews' : 2 // Default number of peer reviews per student in power function
+    'defaultNumberOfReviews' : 2, // Default number of peer reviews per student in power function
     'disablePeerReviewButton' : true,
     'removeGlobalGradesLink' : true,
     'removeGroupsLink' : true
