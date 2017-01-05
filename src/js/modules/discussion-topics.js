4d0229a2c181bc40cf19e38d29a5bd0ee6fe5f00
oppdatert .gitignore og setting.php <stian.kilaas@bouvet.no>
2017-01-05 12:24:41 +0100

    fiks for posted_at undefined

--- src/js/modules/discussion-topics.js
+++ src/js/modules/discussion-topics.js
@@ -3,9 +3,11 @@ this.mmooc = this.mmooc || {};
 this.mmooc.discussionTopics = function () {
     return {
         setDiscussionTopicPubDate: function(discussionTopic) {
-            var formattedDate = mmooc.util.formattedDate(discussionTopic.posted_at);
-            var pubDate = $("<div class='publication-date'>" + formattedDate + "</div>");
-            $(pubDate).prependTo('#discussion_topic .discussion-header-right');
+            if(discussionTopic.posted_at) {
+              var formattedDate = mmooc.util.formattedDate(discussionTopic.posted_at);
+              var pubDate = $("<div class='publication-date'>" + formattedDate + "</div>");
+              $(pubDate).prependTo('#discussion_topic .discussion-header-right');
+            }
         },
 
         alterDiscussionReadStateLook: function() {
