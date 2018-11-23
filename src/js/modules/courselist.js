c77c4f119f2dd5c52dd307aa07765b287a80135d
kubazalewski <jakub.zalewski@netguru.pl>
2018-11-23 13:57:50 +0100

    refactor courselists.js file

--- src/js/modules/courselist.js
+++ src/js/modules/courselist.js
@@ -1,125 +1,105 @@
 this.mmooc = this.mmooc || {};
 
-this.mmooc.courseList = (function() {
+this.mmooc.courseList = (() => {
   return {
-    listCourses: function(parentId, callback) {
+    listCourses(parentId, callback) {
       if (document.getElementsByClassName('reaccept_terms').length === 0) {
-        mmooc.api.getEnrolledCourses(function(courses) {
-          var $oldContent = $('#' + parentId).children(); //After an update the 'Add course button' is in #content including a popupform. So we need to move this to another place in the DOM so we don't overwrite it.
+        mmooc.api.getEnrolledCourses(courses => {
+          const $oldContent = $(`#${parentId}`).children(); //After an update the 'Add course button' is in #content including a popupform. So we need to move this to another place in the DOM so we don't overwrite it.
           $oldContent.appendTo('#right-side-wrapper #right-side');
-
-          $('#' + parentId).html(
-            '<div>Laster ' +
-              mmooc.i18n.CoursePlural.toLowerCase() +
-              '....</div>'
+          $(`#${parentId}`).html(
+            `<div>Laster ${mmooc.i18n.CoursePlural.toLowerCase()}....</div>`
           ); //overwrite the contents in parentID and display: 'Laster kurs....'
-
-          var html = '';
-
+          let html = '';
           if (courses.length == 0) {
-            html =
-              '<h1>Mine ' +
-              mmooc.i18n.CoursePlural.toLowerCase() +
-              '</h1>' +
-              '<p>' +
-              mmooc.i18n.NoEnrollments +
-              '</p>' +
-              "<a class='btn' href='/search/all_courses'>Se tilgjengelige " +
-              mmooc.i18n.CoursePlural.toLowerCase() +
-              '</a>';
-            $('#' + parentId).html(html);
+            html = `<h1>Mine ${mmooc.i18n.CoursePlural.toLowerCase()}</h1><p>${
+              mmooc.i18n.NoEnrollments
+            }</p><a class='btn' href='/search/all_courses'>Se tilgjengelige ${mmooc.i18n.CoursePlural.toLowerCase()}</a>`;
+            $(`#${parentId}`).html(html);
           } else {
             html = mmooc.util.renderTemplateWithData('courselistcontainer', {
               courseLabel: mmooc.i18n.CoursePlural.toLowerCase()
             });
-            $('#' + parentId).html(html);
-
-            var sortedCourses = mmooc.util.arraySorted(courses, 'course_code');
-
-            var categorys = mmooc.util.getCourseCategories(sortedCourses);
-
-            var coursesCategorized = mmooc.util.getCoursesCategorized(
+            $(`#${parentId}`).html(html);
+            const sortedCourses = mmooc.util.arraySorted(
+              courses,
+              'course_code'
+            );
+            const categorys = mmooc.util.getCourseCategories(sortedCourses);
+            const coursesCategorized = mmooc.util.getCoursesCategorized(
               sortedCourses,
               categorys
             );
 
-            for (var i = 0; i < coursesCategorized.length; i++) {
+            coursesCategorized.forEach(course => {
               html = mmooc.util.renderTemplateWithData('courselist', {
-                title: coursesCategorized[i].title,
-                courses: coursesCategorized[i].courses,
+                title: course.title,
+                courses: course.courses,
                 courseLabel: mmooc.i18n.Course.toLowerCase()
               });
               $('.mmooc-course-list-container').append(html);
-            }
+            });
           }
           document.title = mmooc.i18n.CoursePlural;
 
-          if ($.isFunction(callback)) {
-            callback();
-          }
+          if ($.isFunction(callback)) callback();
         });
       }
     },
-    showAddCourseButton: function() {
+    showAddCourseButton() {
       // Move canvas Start new course button, since we hide its original location
-      var $button = $('#start_new_course');
+      const $button = $('#start_new_course');
       if ($button.length) {
         $('#content').append($button);
         $button.html(mmooc.i18n.AddACourse);
       }
     },
-    showFilter: function(sortedCourses) {
+    showFilter(sortedCourses) {
       // Show filter options based on first part of course code
-      var filterOptions = ['Alle'];
-      $(sortedCourses).each(function(index) {
-        var values = sortedCourses[index].course_code.split('::');
+      const filterOptions = ['Alle'];
+      $(sortedCourses).each(index => {
+        const values = sortedCourses[index].course_code.split('::');
         if (values.length > 1) {
-          if (filterOptions.indexOf(values[0]) == -1) {
+          if (filterOptions.indexOf(values[0]) == -1)
             filterOptions.push(values[0]);
-          }
         }
       });
       filterOptions.push('Andre');
-      var options = '';
-      for (var i = 0; i < filterOptions.length; i++) {
-        options +=
-          '<option value="' +
-          filterOptions[i] +
-          '">' +
-          filterOptions[i] +
-          '</option>';
-      }
+      const options = '';
+      filterOptions.forEach(option => {
+        options += `<option value="${option}">${option}</option>`;
+      });
       $('#filter').append(options);
     },
-    applyFilter: function(sortedCourses) {
+    applyFilter(sortedCourses) {
       if ($('#filter').val() == 'Alle') {
         $(sortedCourses).each(function() {
-          $('#course_' + this.id).show();
+          $(`#course_${this.id}`).show();
         });
       } else if ($('#filter').val() == 'Andre') {
-        $(sortedCourses).each(function() {
+        $(sortedCourses).each(() => {
           if (this.course_code.indexOf('::') >= 0) {
-            $('#course_' + this.id).hide();
+            $(`#course_${this.id}`).hide();
           } else {
-            $('#course_' + this.id).show();
+            $(`#course_${this.id}`).show();
           }
         });
       } else {
-        $(sortedCourses).each(function() {
-          var courseCode = this.course_code.split('::')[0];
+        $(sortedCourses).each(() => {
+          const courseCode = this.course_code.split('::')[0];
           if ($('#filter').val() == courseCode) {
-            $('#course_' + this.id).show();
+            $(`#course_${this.id}`).show();
           } else {
-            $('#course_' + this.id).hide();
+            $(`#course_${this.id}`).hide();
           }
         });
       }
     },
-    isCourseCompleted: function(modules) {
-      for (var i = 0; i < modules.length; i++) {
-        var module = modules[i];
-        for (var j = 0; j < module.items.length; j++) {
-          var item = module.items[j];
+    isCourseCompleted(modules) {
+      for (let i = 0; i < modules.length; i++) {
+        const module = modules[i];
+        for (let j = 0; j < module.items.length; j++) {
+          const item = module.items[j];
           if (
             item.completion_requirement &&
             !item.completion_requirement.completed
