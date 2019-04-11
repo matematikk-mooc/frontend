c01a0b2bf3b9d892acc49fae23f92a7604e2823b
etsikt <erlend.thune@gmail.com>
2019-04-11 13:04:23 +0200

    Some new files to support root/sub account design.

---
+++ src/js/rootaccount.js
@@ -0,0 +1,67 @@
+// Replace the normal jQuery getScript function with one that supports
+// debugging and which references the script files as external resources
+// rather than inline.
+jQuery.extend({
+  getScript: function(url, callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = url;
+
+    // Handle Script loading
+    {
+      var done = false;
+
+      // Attach handlers for all browsers
+      script.onload = script.onreadystatechange = function() {
+        if (
+          !done &&
+          (!this.readyState ||
+            this.readyState == 'loaded' ||
+            this.readyState == 'complete')
+        ) {
+          done = true;
+          if (callback) callback();
+
+          // Handle memory leak in IE
+          script.onload = script.onreadystatechange = null;
+        }
+      };
+    }
+
+    head.appendChild(script);
+
+    // We handle everything using the script element injection
+    return undefined;
+  }
+});
+
+jQuery(function($) {
+    if(document.location.pathname == "/login/canvas") {
+        mmooc.utilRoot.redirectFeideAuthIfEnrollReferrer();
+    }
+
+    const urlParamsObj = mmooc.utilRoot.urlParamsToObject();
+    const design = urlParamsObj && urlParamsObj['design'];
+    if (design !== undefined && design=="udir") {
+        if(this.udirDesignLoaded === undefined)
+        {
+            this.udirDesignLoaded = true;
+            console.log("Root account:Loading udir design.");
+            $.getScript('https://udirdesignjs');
+
+            var filename = 'https://udirdesigncss';
+
+            var fileref=document.createElement("link")
+            fileref.setAttribute("rel", "stylesheet")
+            fileref.setAttribute("type", "text/css")
+            fileref.setAttribute("href", filename)
+            document.getElementsByTagName("head")[0].appendChild(fileref)
+        } else {
+            console.log("Root account:Udir design already loaded.");
+        }
+    } else {
+        console.log("Root account:Not loading any special design, display application.");
+        $("#application").show();
+    }
+});
+
