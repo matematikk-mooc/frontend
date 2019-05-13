77b12376d784516d7ece94d26f12e0f9b94ae7c3
Dawid Nawrocki <dawiddeveloper@gmail.com>
2019-05-13 02:25:31 +0200

    hash changed to course id

--- src/js/settings.js
+++ src/js/settings.js
@@ -8,7 +8,7 @@ this.mmooc.settings = {
   defaultNumberOfReviews: 1, // Default number of peer reviews per student in power function
   useDataportenGroups : true,
 //  filterCoursesOnAccountId : [99, 100, 102, 103],
-  filterCoursesOnAccountId: [1, 4, 5],
+  filterCoursesOnAccountId: [4, 5],
   disablePeerReviewButton: true,
   principalRoleType: "Skoleleder",
   removeGlobalGradesLink: true,
