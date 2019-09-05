bed7f0e7f37aaa1f5ed334473bdde3a76d5b7410
etsikt <erlend.thune@gmail.com>
2019-09-05 12:19:12 +0200

    Do not use jQuery to try to support IE11, change Feide login url.

--- src/js/rootaccount.js
+++ src/js/rootaccount.js
@@ -1,8 +1,7 @@
 // Replace the normal jQuery getScript function with one that supports
 // debugging and which references the script files as external resources
 // rather than inline.
-jQuery.extend({
-  getScript: function(url, callback) {
+function getScript(url, callback) {
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.src = url;
@@ -32,46 +31,42 @@ jQuery.extend({
 
     // We handle everything using the script element injection
     return undefined;
-  }
-});
+}
 
-jQuery(function($) {
-    if(document.location.pathname == "/login/canvas") {
-        if (document.referrer.endsWith("/logout" + mmooc.hrefQueryString)) {
-           window.location.href = '/search/all_courses' + mmooc.hrefQueryString;
-        } else if(mmooc.utilRoot.redirectFeideAuthIfEnrollReferrer())
-        {
-            return null;
-        }
-    }
-    else if (document.location.pathname == "/courses") {
-        if(mmooc.utilRoot.redirectToEnrollIfCodeParamPassed())
-        {
-            return null;
-        }
-    }
-    const urlParamsObj = mmooc.utilRoot.urlParamsToObject();
-    const design = urlParamsObj && urlParamsObj['design'];
-    if (design !== undefined && design=="udir") {
-        if(this.udirDesignLoaded === undefined)
-        {
-            this.udirDesignLoaded = true;
-            console.log("Root account:Loading udir design.");
-            $.getScript('https://udirdesignjs');
-
-            var filename = 'https://udirdesigncss';
-
-            var fileref=document.createElement("link")
-            fileref.setAttribute("rel", "stylesheet")
-            fileref.setAttribute("type", "text/css")
-            fileref.setAttribute("href", filename)
-            document.getElementsByTagName("head")[0].appendChild(fileref)
-        } else {
-            console.log("Root account:Udir design already loaded.");
-        }
+//Redirect if necessary
+var redirected = false;
+if(document.location.pathname == "/login/canvas") {
+    if (document.referrer.endsWith("/logout" + mmooc.hrefQueryString)) {
+       window.location.href = '/search/all_courses' + mmooc.hrefQueryString;
+       redirected = true;
     } else {
-        console.log("Root account:Not loading any special design, display application.");
-        $("#application").show();
+      redirected = mmooc.utilRoot.redirectFeideAuthIfEnrollReferrer();
     }
-});
+}
+else if (document.location.pathname == "/courses") {
+    redirected = mmooc.utilRoot.redirectToEnrollIfCodeParamPassed();
+} 
+if(!redirected) {
+  const urlParamsObj = mmooc.utilRoot.urlParamsToObject();
+  const design = urlParamsObj && urlParamsObj['design'];
+  if (design !== undefined && design=="udir") {
+      if(this.udirDesignLoaded === undefined)
+      {
+          this.udirDesignLoaded = true;
+          console.log("Root account:Loading udir design.");
+          getScript('https://udirdesignjs');
 
+          var filename = 'https://udirdesigncss';
+
+          var fileref=document.createElement("link")
+          fileref.setAttribute("rel", "stylesheet")
+          fileref.setAttribute("type", "text/css")
+          fileref.setAttribute("href", filename)
+          document.getElementsByTagName("head")[0].appendChild(fileref)
+      } else {
+          console.log("Root account:Udir design already loaded.");
+      }
+  } else {
+      console.log("Root account:Not loading any special design, display application.");
+  }
+}
