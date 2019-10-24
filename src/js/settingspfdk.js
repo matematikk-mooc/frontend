92d95204de616aadb7ff1c85d2999b23fb7a5ca5
etsikt <erlend.thune@gmail.com>
2019-10-24 15:27:44 +0200

    Add settings for PfDK.

---
+++ src/js/settingspfdk.js
@@ -0,0 +1,16 @@
+this.mmooc=this.mmooc||{};
+
+
+this.mmooc.settings = {
+    'CanvaBadgeProtocolAndHost' : 'https://canvabadges-beta-iktsenteret.bibsys.no',
+    'useCanvaBadge' : false,
+    'defaultNumberOfReviews' : 1, // Default number of peer reviews per student in power function
+    'disablePeerReviewButton' : true,
+    'removeGlobalGradesLink' : true,
+    'displayDiscussionsTab' : true,
+    'displayGroupsTab' : true,
+    'help' : true,
+    'removeGroupsLink' : true,
+    'privacyPolicyLink' : 'http://matematikk-mooc.github.io/privacypolicy.html',
+    'platformName' : 'usn.instructure.com'
+};
---
+++ src/js/settingsudir.js
@@ -0,0 +1,25 @@
+this.mmooc = this.mmooc || {};
+
+// Course ID for selected course, which frontend page
+// will be swapped with All Courses list
+this.mmooc.settings = {
+  CanvaBadgeProtocolAndHost: 'https://canvabadges-beta-iktsenteret.bibsys.no',
+  useCanvaBadge: false,
+  defaultNumberOfReviews: 1, // Default number of peer reviews per student in power function
+  useDataportenGroups : false,
+  filterCourses: true,
+  filterCoursesOnAccountId: [99, 100, 102, 103, 137, 138, 139, 145],
+  disablePeerReviewButton: false,
+  principalRoleType: "Skoleleder",
+  removeGlobalGradesLink: true,
+  removeGroupsLink: true,
+  displayGroupsTab: false,
+  displayDiscussionsTab : false,
+  displayCallForAssistanceButtonInGroupDisccussions : false,
+  privacyPolicyLink: 'https://kompetanseudirno.azureedge.net/udirdesign/privacypolicy.html',
+  contactPoint: 'kompetansesupport@udir.no',
+  platformName: 'UDIR - kompetanseplattform',
+  homeOrganization: 'Udir.no',
+  aboutThePlatform: 'https://kompetanseudirno.azureedge.net/udirdesign/omkompetanseudirno.html'
+};
+
