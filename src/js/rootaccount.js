5efb65a29f1678fe7f7ca0b08ef4f84393d65373
etsikt <erlend.thune@gmail.com>
2020-08-10 12:53:44 +0200

    Remove dialog box when logging in.

--- src/js/rootaccount.js
+++ src/js/rootaccount.js
@@ -2,64 +2,52 @@
 // debugging and which references the script files as external resources
 // rather than inline.
 function getScript(url, callback) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = url;
+  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+  script.src = url;
 
-    // Handle Script loading
-    {
-      var done = false;
+  // Handle Script loading
+  {
+    var done = false;
 
-      // Attach handlers for all browsers
-      script.onload = script.onreadystatechange = function() {
-        if (
-          !done &&
-          (!this.readyState ||
-            this.readyState == 'loaded' ||
-            this.readyState == 'complete')
-        ) {
-          done = true;
-          if (callback) callback();
+    // Attach handlers for all browsers
+    script.onload = script.onreadystatechange = function() {
+      if (
+        !done &&
+        (!this.readyState ||
+          this.readyState == 'loaded' ||
+          this.readyState == 'complete')
+      ) {
+        done = true;
+        if (callback) callback();
 
-          // Handle memory leak in IE
-          script.onload = script.onreadystatechange = null;
-        }
-      };
-    }
+        // Handle memory leak in IE
+        script.onload = script.onreadystatechange = null;
+      }
+    };
+  }
 
-    head.appendChild(script);
+  head.appendChild(script);
 
-    // We handle everything using the script element injection
-    return undefined;
+  // We handle everything using the script element injection
+  return undefined;
 }
 
 //Redirect if necessary
 var redirected = false;
 if(document.location.pathname == "/login/canvas") {
-    $(".ic-Login").hide();
+  if (document.referrer.endsWith("/logout" + mmooc.hrefQueryString)) {
+     window.location.href = '/search/all_courses' + mmooc.hrefQueryString;
+     redirected = true;
+  } else {
     redirected = mmooc.utilRoot.redirectFeideAuthIfEnrollReferrer();
-    if(!redirected) {
-      if(!document.location.search.includes("normalLogin=1")) {
-        let html = '<div class="login-box"><div class="login-box__upper"><p class="login-box__text">Logg inn på kompetanseportalen</p>\
-        <div class="login-box__close"></div></div><div class="login-box__lower">\
-        <a class="mmooc-button mmooc-button-primary" onclick="window.location.href=\'/login/saml\'">&nbsp;\
-        </a><a class="mmooc-button mmooc-button-secondary" onclick="$(\'.login-box, .overlay\').remove(); $(\'.ic-Login\').show()">Har ikke Feide</a></div></div>';
-        
-        document.getElementById('wrapper').insertAdjacentHTML('afterend', html);
-        $(".login-box__close").hide();
-        $('#application').before(`<div class="overlay"></div>`);
-      }
-      else {
-        $(".ic-Login").show();
-      }
-    }
+  }
 }
 else if (document.location.pathname == "/courses") {
-    redirected = mmooc.utilRoot.redirectToEnrollIfCodeParamPassed();
+  redirected = mmooc.utilRoot.redirectToEnrollIfCodeParamPassed();
 } else {
-    redirected = mmooc.utilRoot.redirectToSamlIfUdirCourse(mmooc.settingsRoot.kpasApiUrl);
+  redirected = mmooc.utilRoot.redirectToSamlIfUdirCourse(mmooc.settingsRoot.kpasApiUrl);
 }
-
 if(!redirected) {
   const urlParamsObj = mmooc.utilRoot.urlParamsToObject();
   const design = urlParamsObj && urlParamsObj['design'];
@@ -84,4 +72,3 @@ if(!redirected) {
       console.log("Root account:Not loading any special design.");
   }
 }
-
