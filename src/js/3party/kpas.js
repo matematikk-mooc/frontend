e4036f6399c39c8c2a96059863dfebe32eba4671
etsikt <erlend.thune@gmail.com>
2020-10-06 16:18:53 +0200

    Add style. Add template.

--- src/js/3party/kpas.js
+++ src/js/3party/kpas.js
@@ -1,3 +1,5 @@
+//D3 code from https://bl.ocks.org/hrecht/f84012ee860cb4da66331f18d588eee3
+
 this.mmooc = this.mmooc || {};
 
 //https://medium.com/@pointbmusic/youtube-api-checklist-c195e9abaff1
@@ -5,7 +7,17 @@ this.mmooc.kpas = (function() {
     var hrefPrefix = 'https://statistics-api.azurewebsites.net/api/statistics/';
 
     return {
-        createDiagram : function(data, elementId) {
+        createDiagram : function(data, elementId, name) {
+            var graphicId = name+ "-graphic".trim();
+            var htmlCode = mmooc.util.renderTemplateWithData(
+                'statistics',
+                {name:name,
+                graphicId:graphicId}
+              );
+              document
+                .getElementById(elementId)
+                .insertAdjacentHTML('afterbegin', htmlCode);
+
             //set up svg using margin conventions - we'll need plenty of room on the left for labels
             var margin = {
                 top: 15,
@@ -17,7 +29,7 @@ this.mmooc.kpas = (function() {
             var width = 960 - margin.left - margin.right,
                 height = 500 - margin.top - margin.bottom;
     
-            var svg = d3.select(elementId).append("svg")
+            var svg = d3.select("#"+graphicId).append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
@@ -101,7 +113,7 @@ this.mmooc.kpas = (function() {
                     return d3.ascending(a.enrollment_percentage_category, b.enrollment_percentage_category);
                 })
     
-                mmooc.kpas.createDiagram(data, "#kommune-statistikk");
+                mmooc.kpas.createDiagram(data, "kommune-statistikk", result.Result[0].municipality_name);
             });
             return null;
         },
@@ -120,7 +132,7 @@ this.mmooc.kpas = (function() {
                     return d3.ascending(a.enrollment_percentage_category, b.enrollment_percentage_category);
                 })
     
-                mmooc.kpas.createDiagram(data, "#fylke-statistikk");
+                mmooc.kpas.createDiagram(data, "fylke-statistikk", result.Result[0].county_name);
             });
             return null;
         },
