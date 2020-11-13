85f91093936cb309e4a5e7f58535a65cdea22bab
Mathias M Nilsen <mathias.meier.nilsen@udir.no>
2020-11-13 09:27:06 +0100

    google tag manager global og head

---
+++ src/js/3party/global-canvas-gtm.js
@@ -0,0 +1,7 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JFK0WJJVMX"></script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JFK0WJJVMX');
