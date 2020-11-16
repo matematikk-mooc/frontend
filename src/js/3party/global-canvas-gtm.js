a8c3a6264dcfd3b7b55b2ba66871adf8b2d7580c
Mathias M Nilsen <mathias.meier.nilsen@udir.no>
2020-11-16 13:55:52 +0100

    backup for switch branch

--- src/js/3party/global-canvas-gtm.js
+++ src/js/3party/global-canvas-gtm.js
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JFK0WJJVMX"></script>
+/*<script async src="https://www.googletagmanager.com/gtag/js?id=G-JFK0WJJVMX"></script>*/
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
