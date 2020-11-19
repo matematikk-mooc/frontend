0e933d0f12030312d7066c536f2d7d5b9dd2b12c
Mathias M Nilsen <mathias.meier.nilsen@udir.no>
2020-11-19 14:56:23 +0100

    google tag for test container inserted and fixed

--- src/js/3party/global-canvas-gtm.js
+++
@@ -1,7 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-/*<script async src="https://www.googletagmanager.com/gtag/js?id=G-JFK0WJJVMX"></script>*/
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JFK0WJJVMX');
