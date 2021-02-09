df27016fd76db5e37cb015cbaca48d2657a7b750
Mathias M. Nilsen <Mathias.meier.nilsen@udir.no>
2021-02-09 11:00:56 +0100

    removed wrong syntax gtm_v6_head

--- src/js/3party/gtm_v6_head.js
+++ src/js/3party/gtm_v6_head.js
@@ -2,6 +2,6 @@
 new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0],
  j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = 
  true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
- })(window, document, 'script', 'dataLayer', 'GTM-KWDBBFX'); ‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍‍
-
+ })(window, document, 'script', 'dataLayer', 'GTM-KWDBBFX');
+ 
  var googleTagManager = '<!-- Google Tag Manager (noscript) -->' +    '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWDBBFX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>' +    '<!-- End Google Tag Manager (noscript) -->';$('body').prepend(googleTagManager);
