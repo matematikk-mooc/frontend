401cc8b015f6f049bf7570ae2d24bcf6a0df7bad
Erlend Thune <erlend.thune@udir.no>
2021-11-16 12:07:44 +0100

    _get now calls custom error callback if defined._delete method added in api. Overlay now works on scrollable pages. Add privacy policy accepted dialog box that communicates with KPAS.

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -37,6 +37,7 @@ this.mmooc.api = (function() {
       const uri = this._uriPrefix + options.uri;
       const params = options.params || {};
       const callback = options.callback;
+      const customError = options.error;
 
       $.ajax({
         url: uri,
@@ -62,6 +63,9 @@ this.mmooc.api = (function() {
         },
         error(XMLHttpRequest, textStatus, errorThrown) {
           console.log('Error during GET');
+          if(customError) {
+            customError(XMLHttpRequest.responseText);
+          }
         }
       });
     },
@@ -84,6 +88,24 @@ this.mmooc.api = (function() {
         },
         error(XMLHttpRequest, textStatus, errorThrown) {
           console.log('Error during PUT');
+        }
+      });
+    },
+
+    _delete(options) {
+      const uri = this._uriPrefix + options.uri;
+      const params = options.params || {};
+      const callback = options.callback;
+
+      $.ajax({
+        url: uri,
+        type: 'DELETE',
+        data: params,
+        success(response) {
+          callback(response);
+        },
+        error(XMLHttpRequest, textStatus, errorThrown) {
+          console.log('Error during DELETE');
         }
       });
     },
@@ -994,20 +1016,34 @@ $canvas.post(uri, {'enrollment[user_id]' => user_id, 'enrollment[type]' => etype
         params: params
       });
     },
+    deleteUserCustomData(callback) {
+      this._delete({
+        callback: callback,
+        uri: `/users/self/custom_data`,
+        params: {  
+          "ns": "no.udir.kompetanse"
+        }
+      });
+    },
 
-    savePrivacyPolicyAccepted(version, callback) {
+    saveUserCustomData(privacyPolicyVersion, callback) {
       this._put({
         callback: callback,
         uri: `/users/self/custom_data`,
         params: {  
           "ns": "no.udir.kompetanse",
           "data": {
-            "privacyPolicyVersion": version,
+            "privacyPolicyVersion": privacyPolicyVersion,
           }
         }
       });
     },
-    loadPrivacyPolicyAccepted(callback, error) {
+
+    saveUserPrivacyPolicyVersion(privacyPolicyVersion, callback) {
+      this.saveUserCustomData(privacyPolicyVersion, callback);
+    },
+
+    loadUserCustomData(callback, error) {
       this._get({
         callback: callback,
         error: error,
@@ -1016,6 +1052,10 @@ $canvas.post(uri, {'enrollment[user_id]' => user_id, 'enrollment[type]' => etype
           "ns": "no.udir.kompetanse",
         }
       });
+    },
+
+    loadUserPrivacyPolicyVersion(callback, error) {
+      this.loadUserCustomData(callback, error);
     },
 
     getCaledarEvents(params, callback) {
