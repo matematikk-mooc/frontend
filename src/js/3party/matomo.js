7d42a0bb8099e4f583b1b15ca9fd7d18e1f44fff
Audun Lillekjendlie <24764454+audunjl@users.noreply.github.com>
2022-05-30 12:53:24 +0200

    Fix matomo tracking

--- src/js/3party/matomo.js
+++ src/js/3party/matomo.js
@@ -1,11 +1,14 @@
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setCookieDomain", "*.kompetanse.udir.no"]);
+_paq.push(["setDomains", ["*.kompetanse.udir.no","*.bibsys.instructure.com","*.bibsys.test.instructure.com"]]);
+_paq.push(["enableCrossDomainLinking"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
     var u="//statistik.digilaer.no/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
+    _paq.push(['setSiteId', '3']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
