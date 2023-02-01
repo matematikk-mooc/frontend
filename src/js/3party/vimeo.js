2b3193dd51f0a7212fd912b05e702c7f26782ffa
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2023-02-01 13:35:13 +0100

    KURSP-677-selector-label
    add label to transcription language dropdown

--- src/js/3party/vimeo.js
+++ src/js/3party/vimeo.js
@@ -320,7 +320,8 @@ this.mmooc.vimeo = function() {
 				s.setAttribute("id", transcriptSelectId);
 				s.setAttribute("class", "vimeoSelectLanguage");
 				s.setAttribute("style", "display: none;");
-	
+				s.setAttribute("aria-label", "Velg språk");
+
 				var html = mmooc.util.renderTemplateWithData('transcriptMenu', {
 					transcriptSelectId: transcriptSelectId,
 					languageTracks: tracks,
