4fb25bb372c94bd6b5bc9de588d737152e50e6a3
ThereseRingPersen <therese.persen@me.com>
2023-08-23 12:53:49 +0200

    add webpack config for dev and remove unnecessary code

--- src/vue/app.js
+++ src/vue/app.js
@@ -1,37 +1,34 @@
 import FooterTest from './components/FooterTest.vue';
-import TestHeader from './components/TestHeader.vue';
-import { addStylesheet } from './style-injector';
-import api from '../js/api/api';
-import { createApp } from 'vue/dist/vue.runtime.esm-browser.prod.js'
-import util from '../js/modules/util';
+import TestHeader from "./components/TestHeader.vue";
+import api from "../js/api/api";
+import { createApp } from "vue/dist/vue.runtime.esm-browser.prod.js";
+import util from "../js/modules/util";
 import "./design/index.scss";
 
-
 try {
-    console.error('Hello we are adding styles')
-    addStylesheet();
-    api.getCoursesForAccount(138, function(courses) {
-        console.log(courses);
-        let footerProps =  {
-            "name": "NameProp",
-            "licence": util.isMMOOCLicense(),
-            "courses": courses,
-        }
+  api.getCoursesForAccount(138, function (courses) {
+    console.log(courses);
+    let footerProps = {
+      name: "NameProp",
+      licence: util.isMMOOCLicense(),
+      courses: courses,
+    };
 
-        var footerComponent = createApp(FooterTest, footerProps);
-        var footerWrapper = document.getElementById("wrapper").appendChild(document.createElement("div"));
-        footerWrapper.setAttribute("id", "footer");
-        footerComponent.mount('#footer');
-})
+    var footerComponent = createApp(FooterTest, footerProps);
+    var footerWrapper = document
+      .getElementById("wrapper")
+      .appendChild(document.createElement("div"));
+    footerWrapper.setAttribute("id", "footer");
+    footerComponent.mount("#footer");
+  });
 
-    var header = createApp(TestHeader);
-    var headerwrapper = document.getElementById("application").children[0];
-    headerwrapper.append(document.createElement("div"));
-    headerwrapper.setAttribute("id", "test2");
-    header.mount('#test2');
-
+  var header = createApp(TestHeader);
+  var headerwrapper = document.getElementById("application").children[0];
+  headerwrapper.append(document.createElement("div"));
+  headerwrapper.setAttribute("id", "test2");
+  header.mount("#test2");
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 
 export default app;
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -8,214 +8,207 @@ const TerserPlugin = require('terser-webpack-plugin');
 const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
-
-    entry: {
-        // ...other entry points...
-        'rootaccount-localhost': [
-            './src/js/settingsRoot.js',
-            './src/js/utilRoot.js',
-            './src/js/rootaccount.js',
-        ],
-
-        'subaccount-localhost': [
-            './src/js/settingsRoot.js',
-            './src/js/utilRoot.js',
-            './src/js/subaccount.js',
-            './src/vue/design/colors.scss'
-        ],
-        'kompetanseportalen-localhost': [
-            './src/js/i18n.js',
-            './src/js/main.js',
-            './src/vue/app.js',
-        ],
-        'badges-dev': [
-            './src/addons/badges/js/main.js',
-            './src/js/modules/template.js',
-            './src/js/modules/util.js',
-            './src/js/i18n.js',
-            './src/js/settings.js',
-        ],
-    },
-
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['dist', 'tmp', 'babel', 'replace', ],
-            cleanAfterEveryBuildPatterns: ['dist/*.js.map'],
-
-        }),
-        new webpack.DefinePlugin({
-            'VUECSS': JSON.stringify('index.css'),
-            'DESIGNCSS' : JSON.stringify('subaccount-localhost.css'),
-            'DESIGNJS' : JSON.stringify('kompetanseportalen-localhost.js'),
-            'SERVER': JSON.stringify('http://localhost:9000/'),
-            'KPASAPIURL': JSON.stringify('https://kpas.staging.kompetanse.udir.no/api'),
-            'ACCOUNTID' : [99, 100, 102, 103, 137, 138, 139, 145],
-            'KPAS_MERGE_LTI_ID' : 863,
-            'KPAS_IFRAME_VERSION' : JSON.stringify('localhost'),
-            __VUE_OPTIONS_API__: 'true',
-            __VUE_PROD_DEVTOOLS__: 'false'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css', // Output CSS filenames
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'src/vue/assets/fonts/*.woff',
-                    to: 'fonts/[name][ext]'
-                },
-                {
-                    from: 'src/vue/assets/fonts/*.woff2',
-                    to: 'fonts/[name][ext]'
-                },
-                {
-                    from: 'src/bitmaps/*',
-                    to: 'bitmaps/[name][ext]'
-                },
-                {
-                    from: 'src/vector_images/*',
-                    to: 'vector_images/[name][ext]'
-                },
-                {
-                    from: 'kpas/*',
-                    to: 'kpas/[name][ext]'
-                }
-            ]
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new VueLoaderPlugin(),
+  entry: {
+    // ...other entry points...
+    "rootaccount-localhost": [
+      "./src/js/settingsRoot.js",
+      "./src/js/utilRoot.js",
+      "./src/js/rootaccount.js",
     ],
-    module: {
-        rules: [
-            // Add loaders for different file types
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-            {
-                test: /\.hbs$/,
-                loader:'handlebars-loader',
-                options: {
-                    helperDirs: path.resolve(__dirname, 'src/js/modules/template.js'),
-                    precompileOptions: {
-                        knownHelpersOnly: false
-                    },
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: "sass-loader",
-                        options: {
-                          implementation: require("sass"),
-                          sassOptions: {
-                            additionalData: JSON.stringify('http://localhost:9000/') + ';',
-                            includePaths: ["src/vue/*", "absolute/path/b"],
-                          },
-                        },
-                      },
-              
-                    ]
-             },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            lessOptions: {
-                                relativeUrls: false,
-                                globalVars: {
-                                    SERVER: JSON.stringify('http://localhost:9000/'),
 
-                                },
-                            }
-                        }
-                    },
-                ],
-            },
-            {
-                test: /\.vue$/,
-                loader: "vue-loader",
-                options: {
-                    extractCSS: true
-                },
-                exclude: /node_modules/
-            }
-        ],
-    },
-    resolve: {
-        alias: {
-            setup: path.resolve(__dirname, 'src/css/setup'),
-            Handlebars: path.resolve('src/3party/handlebars-v1.3.0.js'),
-            vue$: path.resolve("node_modules/vue/dist/vue.esm-bundler.js")
-        },
-        extensions: ['.js', '.less', '.hbs', '.vue'],
-        preferRelative: true,
-        modules: ["src", "node_modules"],
+    "subaccount-localhost": [
+      "./src/js/settingsRoot.js",
+      "./src/js/utilRoot.js",
+      "./src/js/subaccount.js",
+      "./src/vue/design/colors.scss",
+    ],
+    "kompetanseportalen-localhost": [
+      "./src/js/i18n.js",
+      "./src/js/main.js",
+      "./src/vue/app.js",
+    ],
+    "badges-dev": [
+      "./src/addons/badges/js/main.js",
+      "./src/js/modules/template.js",
+      "./src/js/modules/util.js",
+      "./src/js/i18n.js",
+      "./src/js/settings.js",
+    ],
+  },
 
-    },
-
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(
-                {
-                    parallel: true,
-                    extractComments: false
-                }
-            )
-        ],
-    },
-
-    devServer: {
-        client: {
-            overlay: {
-                errors: true,
-                warnings: false,
-                runtimeErrors: false,
-            },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["dist", "tmp", "babel", "replace"],
+      cleanAfterEveryBuildPatterns: ["dist/*.js.map"],
+    }),
+    new webpack.DefinePlugin({
+      VUECSS: JSON.stringify("index.css"),
+      DESIGNCSS: JSON.stringify("subaccount-localhost.css"),
+      DESIGNJS: JSON.stringify("kompetanseportalen-localhost.js"),
+      SERVER: JSON.stringify("http://localhost:9000/"),
+      KPASAPIURL: JSON.stringify("https://kpas.staging.kompetanse.udir.no/api"),
+      ACCOUNTID: [99, 100, 102, 103, 137, 138, 139, 145],
+      KPAS_MERGE_LTI_ID: 863,
+      KPAS_IFRAME_VERSION: JSON.stringify("localhost"),
+      __VUE_OPTIONS_API__: "true",
+      __VUE_PROD_DEVTOOLS__: "false",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css", // Output CSS filenames
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/vue/assets/fonts/*.woff",
+          to: "fonts/[name][ext]",
         },
-        static: {
-            directory: path.resolve(__dirname, 'dist')
+        {
+          from: "src/vue/assets/fonts/*.woff2",
+          to: "fonts/[name][ext]",
         },
-        port: 9000,
-        host: 'localhost',
-        open: true,
-        allowedHosts: 'all',
-        headers: {
-            'Access-Control-Allow-Origin': '*'
+        {
+          from: "src/bitmaps/*",
+          to: "bitmaps/[name][ext]",
         },
-        watchFiles: {
-            paths: ['src'],
+        {
+          from: "src/vector_images/*",
+          to: "vector_images/[name][ext]",
+        },
+        {
+          from: "kpas/*",
+          to: "kpas/[name][ext]",
+        },
+      ],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+  ],
+  module: {
+    rules: [
+      // Add loaders for different file types
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        loader: "handlebars-loader",
+        options: {
+          helperDirs: path.resolve(__dirname, "src/js/modules/template.js"),
+          precompileOptions: {
+            knownHelpersOnly: false,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
             options: {
-                ignored: '**/*.js', // Exclude JavaScript files from triggering a full reload
+              implementation: require("sass"),
+              sassOptions: {
+                additionalData: JSON.stringify("http://localhost:9000/") + ";",
+                includePaths: ["src/vue/*"],
+              },
             },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+            },
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                relativeUrls: false,
+                globalVars: {
+                  SERVER: JSON.stringify("http://localhost:9000/"),
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          extractCSS: true,
         },
-    }
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      setup: path.resolve(__dirname, "src/css/setup"),
+      Handlebars: path.resolve("src/3party/handlebars-v1.3.0.js"),
+      vue$: path.resolve("node_modules/vue/dist/vue.esm-bundler.js"),
+    },
+    extensions: [".js", ".less", ".hbs", ".vue"],
+    preferRelative: true,
+    modules: ["src", "node_modules"],
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        parallel: true,
+        extractComments: false,
+      }),
+    ],
+  },
+
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: false,
+      },
+    },
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 9000,
+    host: "localhost",
+    open: true,
+    allowedHosts: "all",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    watchFiles: {
+      paths: ["src"],
+      options: {
+        ignored: "**/*.js", // Exclude JavaScript files from triggering a full reload
+      },
+    },
+  },
 };
