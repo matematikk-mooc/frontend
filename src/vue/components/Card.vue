270ef5025b21050af702295e04da2e80f750b43f
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2023-09-19 15:52:26 +0200

    KURSP-869: restore font

--- src/vue/components/Card.vue
+++ src/vue/components/Card.vue
@@ -107,7 +107,7 @@ defineProps(['theme', 'courseIllustration'])
   .card-content-title {
 
     h3 {
-      font-size: 1;
+      font-size: 1.125rem;
       font-family: 'Roboto';
       font-weight: 600;
       line-height: 150%;
