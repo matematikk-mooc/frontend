f764964fce9521e0c4900addec1ddfc8e1ca74de
ThereseRingPersen <therese.persen@me.com>
2023-10-02 11:52:58 +0200

    Create login page, and adjust loginform to fit

---
+++ src/vue/pages/login-page/LoginPage.stories.js
@@ -0,0 +1,20 @@
+import LoginPage from './LoginPage.vue' // Adjust the import path as needed
+import LoginForm from '../../components/login-form/LoginForm.vue'
+
+// Export a default object that contains the story title and component
+export default {
+  title: 'Pages/LoginPage',
+  component: LoginPage,
+};
+
+// Define the template for the story
+const Template = (args, { argTypes }) => ({
+  components: { LoginPage, LoginForm },
+  template: '<LoginPage v-bind="$props"><LoginForm></LoginForm> </LoginPage>',
+});
+
+// Define the default args for your component (if any)
+export const Default = Template.bind({});
+Default.args = {};
+
+
---
+++ src/vue/pages/login-page/LoginPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="login-page">
+    <div class="login-page__logo">
+      <div class="login-page__top-container">
+        <img class="login-page__logo" :src="`logo-black.svg`" />
+        <h1 class="login-page__title">Kompetanseportal</h1>
+      </div>
+    
+    </div>
+    
+    <div class="login-page__main-content">
+      <div class="login-page__form"> <slot></slot></div>
+        <div class="login-page__illustration">
+            <img :src="`logg-inn-illustrasjon.svg`">
+        </div>
+    </div>              
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+@import "src/vue/design/colors.scss";
+
+.login-page {
+  position:relative;
+  background-color: $color-grey-100;
+  width: 100%;
+  height: 100%;
+  padding: 0 1.5rem 0 1.5rem;
+
+  &__top-container{
+    display:flex;
+    justify-content: flex-start;
+  }
+ &__title{
+  font-family: Montserrat;
+  font-size: 1.5rem;
+  font-weight: 500;
+ }
+ &__logo{
+  margin-right: 2rem;
+ }
+
+  &__main-content {
+    width: 100%;
+    height: 100%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__illustration {
+   max-width: 30%;
+  }
+  &__form {
+   width: 50%;
+   max-width: 25rem;
+   margin-right: 2.5rem;
+  }
+
+  
+}
+</style>
