22a8c59cf35619c7e4159a54205d7687db8e2d24
ThereseRingPersen <therese.persen@me.com>
2023-10-04 13:54:00 +0200

    Kursp-887: fix images issues
    after refactoring components for use in canvas, the storybook stories were out of date.

--- src/vue/stories/Card.stories.js
+++ src/vue/stories/Card.stories.js
@@ -21,6 +21,7 @@ export const CardComponent = {
 
   args: {
     theme: "theme_0",
+    courseIllustration:"/IllustrasjonerKompetansepakker/Barnehage/DigitaldoemmekraftIBarnehagenMedLederstoette/digital-dommekraft-i-barnehagen-med-lederstotte-liten.svg",
   },
   tags: ["autodocs"],
   argTypes: {
@@ -38,6 +39,30 @@ export const CardComponent = {
         "theme_6",
         "theme_7",
         "theme_8",
+      ],
+    },
+    courseIllustration: {
+      control: {
+        type: "select",
+      },
+      options: [
+        "/IllustrasjonerKompetansepakker/Barnehage/DigitaldoemmekraftIBarnehagenMedLederstoette/digital-dommekraft-i-barnehagen-med-lederstotte-liten.svg",
+        "/IllustrasjonerKompetansepakker/DigitalKompetanseISkolen/DigitalUndervisningPaaSkolenOgHjemme/digital-undervisning-på-skolen-og-hjemme-liten.svg",
+        "/IllustrasjonerKompetansepakker/DigitalKompetanseISkolen/InkluderingOgUniversellUtformingIDigitalPraksis/inkludering-og-universell-utforming-i-digital-praksis-liten.svg",
+        "/IllustrasjonerKompetansepakker/DigitalKompetanseISkolen/PersonvernILaeringsTeknologiForEiereOgLedere/personvern-i-laeringsteknologi-for-eiere-og-ledere-liten.svg",
+        "/IllustrasjonerKompetansepakker/DigitalKompetanseISkolen/TeknologiOgDigitaleFerdigheter/teknologi-og-digitale-ferdigheter-liten.svg",
+        "/IllustrasjonerKompetansepakker/EksamensOgProvesystemet/eksamens-og-provesystemet-liten.svg",
+        "/IllustrasjonerKompetansepakker/Inkludering/EleverMedStorLaeringspotensial/elever-med-stor-laeringspotensial-liten.svg",
+        "/IllustrasjonerKompetansepakker/Inkludering/InkluderendePraksis/inkluderende-praksis-liten.svg",
+        "/IllustrasjonerKompetansepakker/InnforingAvNyeLaereplanerForLaerebedrifterOgProevenemder/NyeLaereplanerForLaerebedrifter/nye-laereplaner-for-laerebedrifter-liten.svg",
+        "/IllustrasjonerKompetansepakker/ProgrammeringOgValgfagForUngdomstrinnet/programmering-valgfag-for-laerere-liten.svg",
+        "/IllustrasjonerKompetansepakker/SFO/TrygtOgGodtMiljoISfo/trygt-og-godt-miljo-i-SFO-liten.svg",
+        "/IllustrasjonerKompetansepakker/Skolemiljo/TrygtOgGodtSkolemiljo/trygt-og-godt-skolemiljo-liten.svg",
+        "/IllustrasjonerKompetansepakker/StotteTilArbeidMedLaereplanverket/InnforingAvNyttLaereplanverk/innfoering-av-nytt-laereplanverk(fagfornyelsen)-liten.svg",
+        "/IllustrasjonerKompetansepakker/StotteTilArbeidMedLaereplanverket/ProgrammeringOgAlgoritmiskTenkning/programmering-og-algoritmisk-tenkning-liten.svg",
+        "/IllustrasjonerKompetansepakker/StotteTilArbeidMedRegelverket/PersonvernISkolen/personvern-i-skolen-liten.svg",
+        "/IllustrasjonerKompetansepakker/StotteTilArbeidMedRegelverket/RegelverkIFagOgYrkesopplaering/regelverk-i-fag-og-yrkesopplaering-liten.svg",
+        "/IllustrasjonerKompetansepakker/StotteTilArbeidMedRegelverket/TospraakligFagopplaering/tospraaaklig-fagopplaering-liten.svg"
       ],
     },
   },
