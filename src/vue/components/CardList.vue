24d45fabb00c014c953bb8d42ac74b7295a13b47
ThereseRingPersen <therese.persen@me.com>
2023-11-03 14:28:34 +0100

    KURSP-924-improve css for the card layout so it can have more cards in the width whenever the screen is large enough

--- src/vue/components/CardList.vue
+++ src/vue/components/CardList.vue
@@ -146,9 +146,16 @@ export default {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  width: 64rem;
+  width: 110rem;
   align-items: flex-start;
   justify-content: flex-start;
+  @media (min-width:1025px){
+    width: 64rem;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+  }
 }
 .card-item {
   margin: 0.25rem 1.5rem 2rem 0;
