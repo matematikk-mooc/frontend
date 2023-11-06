b9c5454b8e19a508ead5169f16c89849d4b7cb67
ThereseRingPersen <therese.persen@me.com>
2023-11-06 13:46:58 +0100

    KURSP-825 main layout created

---
+++ src/vue/components/announcements/announcements.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="course-page-announcements-container">
+  <span>Kunngjøringer</span><span>{{ count }}</span>
+</div>
+</template>
+<script setup>
+const { props } = defineProps(['count']);
+</script>
+<style lang="scss">
+.course-page-announcements-container{
+  display: flex;
+   width: 100%;
+  max-width: 35rem;
+  min-width: 30rem;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 1.25rem;
+  border-radius: 1.25rem 0 0 1.25rem;
+  background: #F5F7F9;
+  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.15);
+}
+</style>
