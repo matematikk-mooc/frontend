5db352a5b99c279e1d339e86331e1e9d31ba0cd1
ThereseRingPersen <therese.persen@me.com>
2023-11-07 14:01:46 +0100

    KURSP-825: created the announcement component link/href style, that links to the announcements

--- src/vue/components/announcements/announcements.vue
+++ src/vue/components/announcements/announcements.vue
@@ -1,22 +1,63 @@
 <template>
-<div class="course-page-announcements-container">
-  <span>Kunngjøringer</span><span>{{ count }}</span>
-</div>
+<a href="link-to-announcements" class="course-page-announcements__container">
+  <h3 class="course-page-announcements__title">
+    <Icon aria-hidden="true" class="announcements-icon" name="campaign" size="2em" />
+    <span>Kunngjøringer</span>
+  </h3>
+  <div class="course-page-announcements__count">{{ count }}</div>
+</a>
 </template>
 <script setup>
+import Icon from '../icon/Icon.vue'
 const { props } = defineProps(['count']);
 </script>
+
 <style lang="scss">
-.course-page-announcements-container{
+@import '../../design/box-shadow';
+@import '../../design/colors.scss';
+.course-page-announcements {
+  &__container{
+  text-decoration:none;
   display: flex;
-   width: 100%;
+  width: 100%;
+  position: relative;
+  font-family: Roboto;
+  color:map-get($color-palette-steel, foreground, 200);
   max-width: 35rem;
   min-width: 30rem;
   align-items: center;
   justify-content: flex-start;
-  padding: 1.25rem;
-  border-radius: 1.25rem 0 0 1.25rem;
-  background: #F5F7F9;
-  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.15);
+  padding: 1rem;
+  border-radius: 1.6875rem 0 0 1.6875rem;
+  border: 0.0625rem solid $color-grey-400; 
+  gap: 0.5rem;
+  background-color: map-get($color-palette-steel, background, 200);
+  @include box-shadow(medium); 
+  }
+  &__title {
+    font-size: 1.125rem;
+    letter-spacing: 0.05em;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    display:flex;
+    align-items:center;
+    gap:0.5rem;
+  }
+
+  &__count {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: -0.25rem;
+    width: 1rem;
+    height: 1rem;
+    font-size: 0.875rem;
+    background-color: map-get($color-palette-red, background, 600);
+    padding: 0.125rem;
+    border-radius: 50%;
+    color: map-get($color-palette-red, foreground, 600);
+  }
 }
 </style>
