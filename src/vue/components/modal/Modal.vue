5f89b7ad3777457449652379382d67c8d529a752
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2023-11-22 14:35:37 +0100

    KURSP-891: login popup, when user clicks sign in

--- src/vue/components/modal/Modal.vue
+++ src/vue/components/modal/Modal.vue
@@ -1,7 +1,7 @@
 <template>
    <Transition name="fade" mode="out-in">
   <div v-if="isOpen"  :key="isOpen ? 'visible' : 'hidden'" class="backdrop" >
-   
+
     <div class="modal">
       <div class="modal__close-button">
         <IconButton @click="closeModal" />
@@ -10,13 +10,13 @@
         <slot name="header" v-if="$slots.header" >
         </slot>
       </div>
-       
+
       <div class="modal__body" v-if="$slots.main" >
         <slot name="main" >
-        
+
         </slot>
       </div>
-     
+
       <div class="modal__actions" v-if="$slots.actions" >
         <slot name="actions">
         </slot>
@@ -50,6 +50,7 @@ const closeModal = () => {
     display: flex;
     justify-content: center;
     align-items: center;
+    z-index: 1000;
 
   }
 .fade-enter-active, .fade-leave-active {
@@ -94,14 +95,16 @@ const closeModal = () => {
       margin-bottom: 1rem;
       word-wrap: break-word;
       padding:0.5rem 0 0.5rem 0;
+      max-width: 40rem;
    }
 
     &__actions {
       position: relative;
+      z-index: 1001;
       width: 100%;
       display:flex;
       justify-content:flex-end;
-       > *  { margin: 0 0.125rem; } 
+       > *  { margin: 0 0.125rem; }
     }
   }
 </style>
