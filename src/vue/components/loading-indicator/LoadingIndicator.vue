b42e260458952b4eb5c0b1a890a0b2445fc720bb
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2023-12-18 15:17:37 +0100

    KURSP-1017: Loading indicator on pages loading courselists

--- src/vue/components/loading-indicator/LoadingIndicator.vue
+++ src/vue/components/loading-indicator/LoadingIndicator.vue
@@ -1,52 +1,52 @@
 <template>
     <div class="loading-container">
-      <div v-for="index in 3" :key="index" class="loading-dot" :class="{ 'dot-active': index === activeDot }"></div>
+        <div v-for="index in 3" :key="index" class="loading-dot" :class="{ 'dot-active': index === activeDot }"></div>
     </div>
-  </template>
+</template>
 
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        activeDot: 1
-      };
+        return {
+            activeDot: 1
+        };
     },
     mounted() {
-      this.startAnimation();
+        this.startAnimation();
     },
     methods: {
-      startAnimation() {
-        this.intervalId = setInterval(() => {
-          this.activeDot = this.activeDot === 3 ? 1 : this.activeDot + 1;
-        }, 300);
-      }
+        startAnimation() {
+            this.intervalId = setInterval(() => {
+                this.activeDot = this.activeDot === 3 ? 1 : this.activeDot + 1;
+            }, 300);
+        }
     },
     beforeDestroy() {
-      clearInterval(this.intervalId);
+        clearInterval(this.intervalId);
     }
-  };
-  </script>
+};
+</script>
 
-  <style scoped lang="scss">
+<style scoped lang="scss">
 @import "../../design/colors.scss";
 
-  .loading-container {
+.loading-container {
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 1.5rem;
     padding-top: 10rem;
-  }
+}
 
-  .loading-dot {
+.loading-dot {
     width: 6rem;
     height: 6rem;
     border-radius: 50%;
     background-color: map-get($color-palette-slate, background, 200);
     transition: background-color 1s ease-in-out;
-  }
+}
 
-  .dot-active {
+.dot-active {
     background-color: map-get($color-palette-slate, background, 400);
-  }
-  </style>
+}
+</style>
