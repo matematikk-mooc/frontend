5330ab7478c62aeba6c1246ea69f7037dd29b2a6
Thea Hvalen Thodesen <thea.hvatho@gmail.com>
2024-03-08 14:38:05 +0100

    KURSP-1125: Fix moduleselector and navigation buttons on discussion pages

--- src/vue/components/course-modules/page-navigation/index.js
+++ src/vue/components/course-modules/page-navigation/index.js
@@ -1,21 +1,24 @@
 import { createApp, h } from "vue";
+
 import PageNavigationButtonLinks from "./PageNavigationButtonLinks.vue";
 
 export function renderPreviousAndNextButton(previousUrl, nextUrl) {
-  removeCanvasButtons();
-  const parentElement = document.getElementsByClassName("module-sequence-footer-content")[0];
-  parentElement.id= "custom-footer-content"
-  let buttonWrapper = document.createElement("div");
-  buttonWrapper.setAttribute('class', "custom-button-navigation-wrapper")
-  buttonWrapper.id = 'custom-button-navigation';
-  parentElement.appendChild(buttonWrapper);
-  parentElement.classList.remove('module-sequence-footer-content')
-  parentElement.classList.add('module-sequence-custom-footer-content')
-  const customPageNavigationButtonLinks = createApp({
-    render: () => h(PageNavigationButtonLinks, { previousUrl, nextUrl}),
-  });
-  customPageNavigationButtonLinks.component(PageNavigationButtonLinks, { previousUrl, nextUrl});
-  customPageNavigationButtonLinks.mount('#custom-button-navigation',{ previousUrl, nextUrl});
+  setTimeout(() => {
+    removeCanvasButtons();
+    let parentElement = document.getElementsByClassName("module-sequence-footer-content")[0];
+    parentElement.id= "custom-footer-content"
+    let buttonWrapper = document.createElement("div");
+    buttonWrapper.setAttribute('class', "custom-button-navigation-wrapper")
+    buttonWrapper.id = 'custom-button-navigation';
+    parentElement.appendChild(buttonWrapper);
+    parentElement.classList.remove('module-sequence-footer-content')
+    parentElement.classList.add('module-sequence-custom-footer-content')
+    const customPageNavigationButtonLinks = createApp({
+      render: () => h(PageNavigationButtonLinks, { previousUrl, nextUrl}),
+    });
+    customPageNavigationButtonLinks.component(PageNavigationButtonLinks, { previousUrl, nextUrl});
+    customPageNavigationButtonLinks.mount('#custom-button-navigation',{ previousUrl, nextUrl});
+  }, 250)
 }
 
 function removeCanvasButtons() {
