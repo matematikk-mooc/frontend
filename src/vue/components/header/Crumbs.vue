b743dc33b074687642e2ec61726207c4cd58885f
Marius Johan Holm Johansen <marius.jh.johansen@gmail.com>
2024-06-19 16:43:35 +0200

    DIT-138: Crumbs.
    - Removed log
    - Changed position, logic and size of arrow icons
    - Changed spacing and layout of items
    - Removed word-breaking
    - Added flex-wrap, items now wrap to newline if not enough space
    - Hidden on windows less than 1100px in size, as seen on udir.no
    - Removed dangling arrow

--- src/vue/components/header/Crumbs.vue
+++ src/vue/components/header/Crumbs.vue
@@ -1,33 +1,33 @@
 <template>
-    <!-- {{ console.log(courses[0]) }} -->
     <nav class="breadcrumb mobile-hide">
-        <p class="breadcrumb-title">Du er her:</p>
         <ul>
+            <li>
+                <p class="breadcrumb-title">Du er her:</p>
+            </li>
             <li v-if=allCourses class="all-courses-crumb">
-                <Icon name="chevron_right" size="1.5em" />
                 <a href="/search/all_courses">Alle kompetansepakker</a>
+                <Icon name="chevron_right" size="1em" />
             </li>
 
             <li v-if="myCourses" class="my-courses-crumb">
-                <Icon name="chevron_right" size="1.5em" />
                 <a :href="coursesLink">Mine kompetansepakker</a>
+                <Icon name="chevron_right" size="1em" />
             </li>
 
             <div v-if="courseTitle" class="specific-course-crumb">
                 <li>
-                    <Icon name="chevron_right" size="1.5em" />
                     <a :href="specificCourseLink">{{ courseTitle }}</a>
+                    <Icon v-if:="domainSubpage" name="chevron_right" size="1em" />
                 </li>
             </div>
             <div v-if="domainSubpage" class="specific-subpage-crumb">
                 <li>
-                    <Icon name="chevron_right" size="1.5em" />
                     <a :href="specificSubpageLink">{{ activeSubpage }}</a>
+                    <Icon v-if:="activeModule" name="chevron_right" size="1em" />
                 </li>
             </div>
             <div v-if="activeModule" class="specific-module-crumb">
                 <li>
-                    <Icon name="chevron_right" size="1.5em" />
                     <a :href="specificModuleLink">{{ activeModule }}</a>
                 </li>
             </div>
@@ -70,35 +70,47 @@ export default {
 </script>
 
 <style scoped>
+@media (max-width: 1100px) {
+    .breadcrumb {
+        display: none !important;
+    }
+}
 .breadcrumb {
     display:flex;
     width:100%;
-    height: 46px;
+    height: 100%;
     background-color: #f1f1f1;
+    text-wrap: nowrap;
     .breadcrumb-title {
         display:flex;
         align-items: center;
-        margin-left: 30px;
-
+        /* margin-bottom: unset; */
     }
     ul {
         display:flex;
         flex-direction: row;
+        flex-wrap: wrap;
         align-items: center;
         list-style: none;
         margin: unset;
+        padding-left: 30px;
         li{
+            &:nth-child(2) {
+            padding-left: 15px;
+        }
         display:flex;
         flex-direction: row;
         align-items: center;
-        margin-left: 12px;
+        margin-bottom: 3px;
+        margin-top: 3px;
             span {
+                padding: 3px 5px 0 5px;
                 display:flex
             }
             a {
-                margin-left: 20px;
                 text-decoration: underline;
             }
+
         }
     }
 }
