75a09f0b1e3d227d8c9ff9dc0295e44d503d5669
Marius <marius.johansen@udir.no>
2025-08-08 01:47:54 +0200

    DIT-700: Playwright fix test

--- tests/canvas/deploy-theme.spec.js
+++ tests/canvas/deploy-theme.spec.js
@@ -26,7 +26,7 @@ describe('Canvas: Theme', async () => {
       await page
         .locator('.ic-ThemeCard-container__Main')
         .locator(
-          '.ic-ThemeCard-main__name button:has-text("' + themeName + '")',
+          '.ic-ThemeCard-main__name span:has-text("' + themeName + '")',
         )
         .waitFor({ state: 'visible' });
     });
