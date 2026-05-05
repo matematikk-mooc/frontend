50bb8efbb0401fe2231f9b5bc5a1d66e215c3f94
Marius <marius.johansen@udir.no>
2026-05-05 14:25:21 +0200

    DIT-33: Fixed filters according to spec.

--- src/vue/pages/LoggedInLandingPage.vue
+++ src/vue/pages/LoggedInLandingPage.vue
@@ -4,7 +4,7 @@
       <div id="main" class="landing-page--content">
         <h1>Alle tilgjengelige kompetansepakker</h1>
         <div class="landing-page--layout">
-          <CardFilter @update:selectedFilters="onSelectedFiltersUpdate" :filterData="filterData"></CardFilter>
+          <CardFilter @update:selectedFilters="onSelectedFiltersUpdate" :filterData="filterData" :courses="courses"></CardFilter>
           <CardList v-if="coursesToView.length > 0" :authorized="true" :courses="coursesToView" :newCoursesIndicator=true></CardList>
           <div class="no-courses-to-show" v-else>
             <h2>Vi fant ingen treff for filtrene du har valgt. Du kan fjerne alle filtrene med "Tilbakestill filter".</h2>
