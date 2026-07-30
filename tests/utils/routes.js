a98f7e735a3efc2f5663b93aa42fbad926ab9637
Marius <marius.johansen@udir.no>
2026-07-30 13:11:33 +0200

    DIT-1033: updated testroute

--- tests/utils/routes.js
+++ tests/utils/routes.js
@@ -3,8 +3,8 @@ const { getCanvasBaseUrl } = require('./shared');
 export const routeToBasicAuth = async (page) => {
   const canvasBaseUrl = getCanvasBaseUrl();
 
-  await page.goto(`${canvasBaseUrl}/login/canvas?normalLogin=1`);
-  await page.waitForSelector('form#login_form', { state: 'visible' });
+  await page.goto(`${canvasBaseUrl}/login/canvas`);
+  await page.getByTestId("login-button").waitFor({ state: "visible" });
 };
 
 export const routeToAuthThemeEditor = async (page) => {
